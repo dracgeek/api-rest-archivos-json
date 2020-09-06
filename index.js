db8b3dcfbbe1e3f1c8d16713399f170f24816cc0
Wolfgang Schubert <david.rodriguez@agileinnova.org>
2020-09-06 16:20:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+
+//Middlewares
+app.use(morgan('dev')); //Monitorear las peticiones
+app.use(express.json()); //Peticiones en formato json
+
+//Routes
+app.use('/api/',require('./routes/movies'));
+
+app.set("port", 4001);
+app.listen(app.get("port"), ()=>{
+    console.log(`Servidor corriendo en el puerto 4001`);
+  });
